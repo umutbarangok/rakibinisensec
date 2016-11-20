f872956271ec5b107e757180151f5fb1a9e2aa04
Zeki Safa Göksu <zsgoksu@gmail.com>
2016-11-20 14:25:39 +0300

    1.0

--- include/menu.php
+++ include/menu.php
@@ -1,8 +1,8 @@
 <nav>
 	<ul>
 			<li><a href="index.php">Anasayfa</a></li>
-			<li><a href="index.php">Anasayfa</a></li>
-			<li><a href="index.php">Anasayfa</a></li>
+			<li><a href="sahalar.php">Sahalar</a></li>
+			<li><a href="admin.php">Admin</a></li>
 			<li><a href="index.php">Anasayfa</a></li>
 			<li><a href="index.php">Anasayfa</a></li>
 	</ul>
