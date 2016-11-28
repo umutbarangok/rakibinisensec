d0fe2f1a1f79a31200964e3cd449b53b5eff4dc4
ALPEREN MEZİRELİ <alperenmezireli@gmail.com>
2016-11-28 15:37:11 +0300

    Delete menu.php

--- include/menu.php
+++
@@ -1,9 +0,0 @@
-<nav>
-	<ul>
-			<li><a href="index.php">Anasayfa</a></li>
-			<li><a href="index.php">Anasayfa</a></li>
-			<li><a href="index.php">Anasayfa</a></li>
-			<li><a href="index.php">Anasayfa</a></li>
-			<li><a href="index.php">Anasayfa</a></li>
-	</ul>
-</nav>
